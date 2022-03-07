061f48f1d7971dfbbc05c0345c0266f443be3f0e
CWNU-djc <100703668+CWNU-djc@users.noreply.github.com>
2022-03-07 14:43:07 +0900

    Create 0307-2.py

---
+++ 0307-2.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+A = np.array([[3, 0], 
+              [-1, 2],
+              [1, 1]])
+              
+B = np.array([[-3, -1], 
+            [2, 1],
+            [4, 3]])
+
+print("A = ", A)
+print("B = ", B)
+
+
+#print(A+2B)
+print("(a)", A+2*B)     
